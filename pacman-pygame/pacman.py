04f684c5d1ba03405768ea9889df3f4b2c3e1749
LugpDev2022 <LugpDev2022@gmail.com>
2025-09-22 09:46:31 -0600

    feat(pacman): rotate pacman with keyboard

--- pacman-pygame/pacman.py
+++ pacman-pygame/pacman.py
@@ -9,12 +9,22 @@ from scripts.animations import show_animation
 init()
 screen = display.set_mode((800, 600))
 
+angle = 0
 while True:
     screen.fill((0, 0, 0))
     for e in event.get():
         if e.type == QUIT: sys.exit()
+        if e.type == KEYDOWN:
+            if e.key == K_w:
+                angle = 90
+            elif e.key == K_s:
+                angle = -90
+            elif e.key == K_a:
+                angle = 180
+            elif e.key == K_d:
+                angle = 0
 
-    images = load_animations("../assets/pacman/pacman", ".png", 3, random.randint(0, 0))
+    images = load_animations("../assets/pacman/pacman", ".png", 3, angle)
     show_animation(images, 5, screen, 100, 100)
 
     display.flip()
