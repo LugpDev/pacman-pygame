65cb75b219d522b99f287855fc1fbab296b79abe
LugpDev2022 <LugpDev2022@gmail.com>
2025-09-25 10:41:46 -0600

    feat: add borders to the map

--- scripts/check_collision.py
+++ scripts/check_collision.py
@@ -3,16 +3,16 @@ PACMAN_SIZE = 30
 def check_collision(pacman):
     for obstacle in pacman["obstacles"]:
         if pacman["angle"] == 0:  # Right
-            collided_y = pacman["y"] in range(obstacle[1], obstacle[3])
+            collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
             collided_x = pacman["x"] + PACMAN_SIZE + pacman["speed"] in range(obstacle[0], obstacle[2])
         elif pacman["angle"] == 90:  # Up
-            collided_y = pacman["y"] - pacman["speed"] in range(obstacle[1], obstacle[3])
+            collided_y = pacman["y"] - pacman["speed"] in range(obstacle[1], obstacle[3] + obstacle[1])
             collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2])
         elif pacman["angle"] == 180:  # Left
-            collided_y = pacman["y"] in range(obstacle[1], obstacle[3])
+            collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
             collided_x = pacman["x"] - pacman["speed"] in range(obstacle[0], obstacle[2])
         elif pacman["angle"] == 270:  # Down
-            collided_y = pacman["y"] + pacman["speed"] + PACMAN_SIZE in range(obstacle[1], obstacle[3])
+            collided_y = pacman["y"] + pacman["speed"] + PACMAN_SIZE in range(obstacle[1], obstacle[3] + obstacle[1])
             collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2])
         else:
             continue
--- scripts/obstacles.py
+++ scripts/obstacles.py
@@ -1,9 +1,15 @@
 def initialize_obstacles(screen_width, screen_height):
-    # [x1, y1, x2, y2]
+    # (x1, y1, x2, y1+y2)
     return [
-        #(0, 0, 13, 250),
-        (screen_width -13 , 0, screen_width, 250),
-        (screen_width - 13, screen_height-300, screen_width, screen_height-50),
-        (0, 0, screen_width, 13),
-        (0, screen_height-13, screen_width, screen_height)
+        # Borders
+        (0, 0, screen_width, 13), # Top
+        (0, 0, 13, 250), # Top Left
+        (screen_width - 13, 0, screen_width, 250), # Top Right
+        (0, 240, 140, 100), # Top Middle Left
+        (screen_width - 140, 240, screen_width, 100), # Top Middle Right
+        (0, 390, 140, 100), # Bottom Middle Left
+        (screen_width - 140, 390, screen_width, 100), # Bottom Middle Right
+        (0, screen_height-300, 13, 300), # Bottom Left
+        (screen_width - 13, screen_height-300, screen_width, screen_height), # Bottom Right
+        (0, screen_height - 13, screen_width, screen_height),  # Bottom
     ]
