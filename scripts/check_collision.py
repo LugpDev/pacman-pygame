e3a04f834374782aa7f4f5147884222dab5d17ef
LugpDev2022 <LugpDev2022@gmail.com>
2025-09-25 10:49:18 -0600

    fix(collision): x2 coordinate was wrong

--- scripts/check_collision.py
+++ scripts/check_collision.py
@@ -4,16 +4,16 @@ def check_collision(pacman):
     for obstacle in pacman["obstacles"]:
         if pacman["angle"] == 0:  # Right
             collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] + PACMAN_SIZE + pacman["speed"] in range(obstacle[0], obstacle[2])
+            collided_x = pacman["x"] + PACMAN_SIZE + pacman["speed"] in range(obstacle[0], obstacle[2] + obstacle[0])
         elif pacman["angle"] == 90:  # Up
             collided_y = pacman["y"] - pacman["speed"] in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2])
+            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2] + obstacle[0])
         elif pacman["angle"] == 180:  # Left
             collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] - pacman["speed"] in range(obstacle[0], obstacle[2])
+            collided_x = pacman["x"] - pacman["speed"] in range(obstacle[0], obstacle[2] + obstacle[0])
         elif pacman["angle"] == 270:  # Down
             collided_y = pacman["y"] + pacman["speed"] + PACMAN_SIZE in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2])
+            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2] + obstacle[0])
         else:
             continue
 
