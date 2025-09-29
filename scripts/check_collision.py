c90d5a1760476efcbd322a658f68c6639a796b4b
LugpDev2022 <LugpDev2022@gmail.com>
2025-09-29 09:33:33 -0600

    fix: obstacle border collisions

--- scripts/check_collision.py
+++ scripts/check_collision.py
@@ -3,17 +3,17 @@ PACMAN_SIZE = 30
 def check_collision(pacman):
     for obstacle in pacman["obstacles"]:
         if pacman["angle"] == 0:  # Right
-            collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
+            collided_y = pacman["y"] in range(obstacle[1] - PACMAN_SIZE, obstacle[3] + obstacle[1])
             collided_x = pacman["x"] + PACMAN_SIZE + pacman["speed"] in range(obstacle[0], obstacle[2] + obstacle[0])
         elif pacman["angle"] == 90:  # Up
             collided_y = pacman["y"] - pacman["speed"] in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2] + obstacle[0])
+            collided_x = pacman["x"] in range(obstacle[0] - PACMAN_SIZE, obstacle[2] + obstacle[0])
         elif pacman["angle"] == 180:  # Left
-            collided_y = pacman["y"] in range(obstacle[1], obstacle[3] + obstacle[1])
+            collided_y = pacman["y"] in range(obstacle[1] - PACMAN_SIZE, obstacle[3] + obstacle[1])
             collided_x = pacman["x"] - pacman["speed"] in range(obstacle[0], obstacle[2] + obstacle[0])
         elif pacman["angle"] == 270:  # Down
             collided_y = pacman["y"] + pacman["speed"] + PACMAN_SIZE in range(obstacle[1], obstacle[3] + obstacle[1])
-            collided_x = pacman["x"] + PACMAN_SIZE in range(obstacle[0], obstacle[2] + obstacle[0])
+            collided_x = pacman["x"] in range(obstacle[0] - PACMAN_SIZE, obstacle[2] + obstacle[0])
         else:
             continue
 
