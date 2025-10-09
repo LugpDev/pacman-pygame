aee7585a5d6dfd0fb1941e8428a93008a9c20bde
LugpDev2022 <LugpDev2022@gmail.com>
2025-10-09 10:10:00 -0600

    feat(phantoms): create the basic movement

--- pacman-pygame/phantoms.py
+++ pacman-pygame/phantoms.py
@@ -2,34 +2,56 @@ from pygame import *
 
 WIDTH, HEIGHT = 35, 30
 
+
 def load_sprite(name):
     sprite = transform.scale(image.load(f'../assets/phantoms/{name}.png'), (WIDTH, HEIGHT))
     return sprite
 
-def create_phantoms():
+
+def create_phantoms(speed):
     return [
         {
-        "name": "orange",
-        "sprite": load_sprite("orange"),
-        "x": 300,
-        "y": 300,
+            "name": "orange",
+            "sprite": load_sprite("orange"),
+            "x": 300,
+            "y": 300,
+            "speed": speed,
         },
         {
             "name": "red",
             "sprite": load_sprite("red"),
             "x": 350,
             "y": 350,
+            "speed": speed,
         },
         {
             "name": "pink",
             "sprite": load_sprite("pink"),
             "x": 300,
             "y": 370,
+            "speed": speed,
         },
         {
             "name": "blue",
             "sprite": load_sprite("blue"),
             "x": 350,
             "y": 390,
+            "speed": speed,
         }
     ]
+
+
+def update_phantom(phantom, dest, obstacles, playing):
+    if not playing:
+        return phantom
+
+    if phantom["x"] < dest["x"]:
+        phantom["x"] += phantom["speed"]
+    elif phantom["x"] > dest["x"]:
+        phantom["x"] -= phantom["speed"]
+    elif phantom["y"] < dest["y"]:
+        phantom["y"] += phantom["speed"]
+    elif phantom["y"] > dest["y"]:
+        phantom["y"] -= phantom["speed"]
+
+    return phantom
